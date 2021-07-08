26a464bec8f9cfb1099dbc6f614fdebdd6a0b0a2
Kristian Freeman <kristian@kristianfreeman.com>
2021-07-08 13:48:48 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center container max-w-lg mx-auto py-12">
+    <h1 class="text-3xl mb-6 font-extrabold">{{headline}}</h1>
+    <p class="text-xl mb-8 mx-8">{{subtitle}}</p>
+    <Links :links="links" />
+  </div>
+</template>
+
+<script setup>
+import Links from './components/Links.vue'
+
+const headline = "Kristian Freeman"
+const subtitle = "Developer advocate and software engineer"
+
+const links = [
+  {
+    name: "Twitter",
+    url: "https://twitter.com/signalnerve"
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/signalnerve"
+  },
+]
+</script>
+
+<style>
+body {
+  background: #1F2937;
+  color: #F9FAFB;
+}
+</style>
---
+++ src/components/Links.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex w-full mb-6" v-for="link in links">
+    <a 
+      class="text-xl rounded-md py-2 px-4 text-center w-full border border-gray-600 hover:bg-gray-500 transition-all" 
+      :href="link.url"
+      target="_blank"
+    >
+      {{link.name}}
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({ links: Array })
+</script>
