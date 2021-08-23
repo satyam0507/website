8126975b131fb4b499ae9999389453aab7eaaec1
Satyam <satyam0507@gmail.com>
2021-08-24 02:02:33 +0530

    added a express server

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function () {
+    console.log('Example app listening on port '+app.get('port'));
+})
+
+app.use(express.static('dist/'));
